e0fcd68df83004cb5f6e92aba97e526971af1907
Shefer Arkadiy <arkadiy.shefer@gmail.com>
2023-04-03 20:52:48 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Forum</title>
+</head>
+<body>
+<div class="header">
+    <div class="container-fluid">
+        <ul class="col-1 offset-10 registration">
+            <li>
+                <button class="log-in">Log in</button>
+            </li>
+            <li>
+                <button class="sign-up">Sign up</button>
+            </li>
+        </ul>
+    </div>
+</div>
+<div class="accountWrap">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-8 mx-auto">
+                <div class="account">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="avatar-wrap">
+                                <img src="images/ShefArk.jpg" alt="Avatar" class="avatar">
+                            </div>
+                        </div>
+                        <div class="col-8 mx-auto">
+                            <table class="table table-striped">
+                                <tr>
+                                    <th>First name</th>
+                                    <td>Arkadii</td>
+                                </tr>
+                                <tr>
+                                    <th>Last name</th>
+                                    <td>Shefer</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="js/main.js"></script>
+</body>
+</html>
